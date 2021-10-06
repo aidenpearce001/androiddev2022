19dc57fa86bf4c123505072d036b066ba68cec67
unknown <aidenpearcewd01@gmail.com>
2021-10-06 11:47:28 +0700

    'update'

---
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -0,0 +1,24 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ForecastFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.main, parent, false);
+//        Fragment fragment = (Fragment) getFragmentManager().findFragmentById(R.id.tv);
+//        fragment.getView().setBackgroundColor(Color.WHITE);
+//        View view = inflater.inflate(R.layout.fragment_weather_and_forecast, container, false);
+//        return view;
+    }
+}
---
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,31 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import android.os.Bundle;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        TextView textView = (TextView)findViewById(R.id.text_message);
+        textView.setText("Hello World");
+    }
+    public void onPause(){
+        super.onPause();
+    }
+    public void onResume(){
+        super.onResume();
+    }
+    public void onStart(){
+        super.onStart();
+    }
+    public void onStop(){
+        super.onStop();
+    }
+    public void onDestroy(){
+        super.onDestroy();
+    }
+}
