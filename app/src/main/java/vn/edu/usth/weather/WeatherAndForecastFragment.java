01fb918c1704433cdeda97cd3d94768193092e31
unknown <aidenpearcewd01@gmail.com>
2021-10-26 16:23:02 +0700

    labwork9

--- app/src/main/java/vn/edu/usth/weather/WeatherAndForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherAndForecastFragment.java
@@ -8,25 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WeatherAndForecastFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class WeatherAndForecastFragment extends Fragment {
 
     public WeatherAndForecastFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment WeatherAndForecastFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    // Here is the code to create new instance for the fragment swipe. Really cool
     public static WeatherAndForecastFragment newInstance() {
         WeatherAndForecastFragment fragment = new WeatherAndForecastFragment();
         Bundle args = new Bundle();
